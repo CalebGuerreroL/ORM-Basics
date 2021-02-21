43342565cbfcc2fd4dd11d32a578bd08cbfc005f
CalebGuerrero <calebguerrero13@gmail.com>
2021-02-20 19:22:06 -0600

    Added environment variables

--- DatabaseFirst_DWB/DataAccess/NorthwindContext.cs
+++ DatabaseFirst_DWB/DataAccess/NorthwindContext.cs
@@ -51,8 +51,7 @@ namespace DatabaseFirst_DWB.DataAccess
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=DESKTOP-VVM0ENV;Database=Northwind;Trusted_Connection=True;");
+                optionsBuilder.UseSqlServer(Environment.GetEnvironmentVariable("CONNECTION_STRING").ToString());
             }
         }
 
